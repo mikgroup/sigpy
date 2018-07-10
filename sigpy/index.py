c79750f858af876a6d8e1cc09a6562722973c7ca
Frank Ong <frankong@berkeley.edu>
2018-07-10 11:18:19 -0700

    added index classes for stochastic optimization.

---
+++ sigpy/index.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+            
+class ShuffledIndex(object):
+    """Produces indices that are shuffled between 0 and n-1.
+
+    Args:
+        n (int): Upper bound on indices.
+    
+    """
+
+    def __init__(self, n):
+        self.stream = np.arange(n)
+        self.next_stream = np.arange(n)
+        
+        np.random.shuffle(self.stream)
+        self.idx = -1
+
+    def current(self):
+        if self.idx == -1:
+            raise TypeError("ShuffledIndex.next() has not been called yet.")
+        
+        return self.stream[self.idx]
+    
+    def next(self):
+        self.idx += 1
+        if self.idx == len(self.stream):
+            np.random.shuffle(self.stream)
+            self.idx = 0
+
+        return self.current()
+
+
+class PingPongIndex(object):
+    """Produces indices that goes back and forth between 0 and n-1.
+
+    For example, if n = 3, it produces {0, 1, 2, 2, 1, 0}.
+
+    Args:
+        n (int): Upper bound on indices.
+    
+    """
+
+    def __init__(self, n):
+        self.n = n
+        self.direction = 1
+        self.idx = -1
+
+    def current(self):
+        if self.idx == -1:
+            raise TypeError("PingPongIndex.next() has not been called yet.")
+        
+        return self.idx
+    
+    def next(self):
+        self.idx += self.direction
+        if self.idx == -1:
+            self.idx = 0
+            self.direction = 1
+        elif self.idx == self.n:
+            self.idx = self.n - 1
+            self.direction = -1
+
+        return self.current()
---
+++ sigpy/index_test.py
@@ -0,0 +1,29 @@
+import pickle
+import unittest
+import numpy as np
+import numpy.testing as npt
+from sigpy import index
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestIndex(unittest.TestCase):
+
+    def test_ShuffledIndex(self):
+
+        n = 5
+        idx = index.ShuffledIndex(n)
+
+        x = sorted([idx.next() for _ in range(2 * n)])
+
+        npt.assert_allclose(x, [0, 0, 1, 1, 2, 2, 3, 3, 4, 4])
+
+    def test_PingPongIndex(self):
+
+        n = 5
+        idx = index.PingPongIndex(n)
+
+        x = [idx.next() for _ in range(2 * n)]
+
+        npt.assert_allclose(x, [0, 1, 2, 3, 4, 4, 3, 2, 1, 0])
