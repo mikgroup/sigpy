8ffeb7aaa9b50c674a90d8daf2fc53cced32c7bb
frankong <frankong@berkeley.edu>
2018-08-20 13:50:43 -0700

    changed release to 0.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='sigpy',
-      version='0.0.2',
+      version='0.0.3',
       description='Python package for signal reconstruction.',
       long_description=long_description,
       long_description_content_type="text/markdown",
