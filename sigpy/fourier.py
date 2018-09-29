27ac63ac9751a4ee78debeab0a3299aa6472b11e
frankong <frankong@berkeley.edu>
2018-09-29 16:48:28 -0700

    Move fft and nufft functions to fourier module
    This is part of on-going process to reduce number of submodules.

---
+++ sigpy/fourier.py
@@ -0,0 +1,344 @@
+# -*- coding: utf-8 -*-
+"""FFT functions.
+
+This module contains FFT functions that support centered operation.
+
+"""
+import numpy as np
+
+from sigpy import backend, config, interp, util
+if config.cupy_enabled:
+    import cupy as cp
+
+
+__all__ = ['fft', 'ifft', 'nufft', 'nufft_adjoint', 'estimate_shape']
+
+
+def fft(input, oshape=None, axes=None, center=True, norm='ortho'):
+    """FFT function that supports centering.
+
+    Args:
+        input (array): input array.
+        oshape (None or array of ints): output shape.
+        axes (None or array of ints): Axes over which to compute the FFT.
+        norm (Nonr or ``"ortho"``): Keyword to specify the normalization mode.
+
+    Returns:
+        array: FFT result of dimension oshape.
+
+    See Also:
+        :func:`numpy.fft.fftn`
+
+    """
+    device = backend.get_device(input)
+    xp = device.xp
+
+    with device:
+        if not np.issubdtype(input.dtype, np.complexfloating):
+            input = input.astype(np.complex)
+
+        if center:
+            output = _fftc(input, oshape=oshape, axes=axes, norm=norm)
+        else:
+            output = xp.fft.fftn(input, s=oshape, axes=axes, norm=norm)
+
+        if np.issubdtype(input.dtype, np.complexfloating) and input.dtype != output.dtype:
+            output = output.astype(input.dtype)
+
+        return output
+
+
+def ifft(input, oshape=None, axes=None, center=True, norm='ortho'):
+    """IFFT function that supports centering.
+
+    Args:
+        input (array): input array.
+        oshape (None or array of ints): output shape.
+        axes (None or array of ints): Axes over which to compute the inverse FFT.
+        norm (None or ``"ortho"``): Keyword to specify the normalization mode.
+
+    Returns:
+        array of dimension oshape.
+
+    See Also:
+        :func:`numpy.fft.ifftn`
+
+    """
+    device = backend.get_device(input)
+    xp = device.xp
+
+    with device:
+        if not np.issubdtype(input.dtype, np.complexfloating):
+            input = input.astype(np.complex)
+
+        if center:
+            output = _ifftc(input, oshape=oshape, axes=axes, norm=norm)
+        else:
+            output = xp.fft.ifftn(input, s=oshape, axes=axes, norm=norm)
+
+        if np.issubdtype(input.dtype, np.complexfloating) and input.dtype != output.dtype:
+            output = output.astype(input.dtype)
+
+        return output
+
+
+def nufft(input, coord, oversamp=1.25, width=4.0, n=128):
+    """Non-uniform Fast Fourier Transform.
+
+    Args:
+        input (array): input array.
+        coord (array): coordinate array of shape (..., ndim). 
+            ndim determines the number of dimension to apply nufft.
+        oversamp (float): oversampling factor.
+        width (float): interpolation kernel full-width in terms of oversampled grid.
+        n (int): number of sampling points of interpolation kernel.
+
+    Returns:
+        array: Fourier domain points of shape input.shape[:-ndim] + coord.shape[:-1]
+
+    References:
+        Fessler, J. A., & Sutton, B. P. (2003). 
+        Nonuniform fast Fourier transforms using min-max interpolation. 
+        IEEE Transactions on Signal Processing, 51(2), 560-574.
+
+        Beatty, P. J., Nishimura, D. G., & Pauly, J. M. (2005). 
+        Rapid gridding reconstruction with a minimal oversampling ratio. 
+        IEEE transactions on medical imaging, 24(6), 799-808.
+
+    """
+    device = backend.get_device(input)
+    xp = device.xp
+    ndim = coord.shape[-1]
+    beta = np.pi * (((width / oversamp) * (oversamp - 0.5))**2 - 0.8)**0.5
+
+    with device:
+        output = input.copy()
+        os_shape = list(input.shape)
+
+        for a in range(-ndim, 0):
+            i = input.shape[a]
+            os_i = _get_ugly_number(oversamp * i)
+            os_shape[a] = os_i
+            idx = xp.arange(i, dtype=input.dtype)
+
+            # Calculate apodization
+            apod = (beta**2 - (np.pi * width * (idx - i // 2) / os_i)**2)**0.5
+            apod /= xp.sinh(apod)
+
+            # Swap axes
+            output = output.swapaxes(a, -1)
+            os_shape[a], os_shape[-1] = os_shape[-1], os_shape[a]
+
+            # Apodize
+            output *= apod
+
+            # Oversampled FFT
+            output = util.resize(output, os_shape)
+            output = fft(output, axes=[-1], norm=None)
+            output /= i**0.5
+
+            # Swap back
+            output = output.swapaxes(a, -1)
+            os_shape[a], os_shape[-1] = os_shape[-1], os_shape[a]
+
+        coord = _scale_coord(backend.to_device(coord, device), input.shape, oversamp)
+        table = backend.to_device(
+            _kb(np.arange(n, dtype=coord.dtype) / n, width, beta, dtype=coord.dtype), device)
+
+        output = interp.interpolate(output, width, table, coord)
+
+        return output
+
+
+def estimate_shape(coord):
+    """Estimate array shape from coordinates.
+
+    Shape is estimated by the different between maximum and minimum of
+    coordinates in each axis.
+
+    Args:
+        coord (array): Coordinates.
+    """
+    ndim = coord.shape[-1]
+    with backend.get_device(coord):
+        shape = [int(coord[..., i].max() - coord[..., i].min()) for i in range(ndim)]
+
+    return shape
+
+
+def nufft_adjoint(input, coord, oshape=None, oversamp=1.25, width=4.0, n=128):
+    """Adjoint non-uniform Fast Fourier Transform.
+
+    Args:
+        input (array): Input Fourier domain array.
+        coord (array): coordinate array of shape (..., ndim). 
+            ndim determines the number of dimension to apply nufft adjoint.
+        oshape (tuple of ints): output shape.
+        oversamp (float): oversampling factor.
+        width (float): interpolation kernel full-width in terms of oversampled grid.
+        n (int): number of sampling points of interpolation kernel.
+
+    Returns:
+        array: Transformed array.
+
+    See Also:
+        :func:`sigpy.nufft.nufft`
+
+    """
+    device = backend.get_device(input)
+    xp = device.xp
+    ndim = coord.shape[-1]
+    beta = np.pi * (((width / oversamp) * (oversamp - 0.5))**2 - 0.8)**0.5
+    if oshape is None:
+        oshape = list(input.shape[:-coord.ndim + 1]) + estimate_shape(coord)
+    else:
+        oshape = list(oshape)
+
+    with device:
+        coord = _scale_coord(backend.to_device(coord, device), oshape, oversamp)
+        table = backend.to_device(
+            _kb(np.arange(n, dtype=coord.dtype) / n, width, beta, dtype=coord.dtype), device)
+        os_shape = oshape[:-ndim] + [_get_ugly_number(oversamp * i) for i in oshape[-ndim:]]
+        output = interp.gridding(input, os_shape, width, table, coord)
+
+        for a in range(-ndim, 0):
+            i = oshape[a]
+            os_i = os_shape[a]
+            idx = xp.arange(i, dtype=input.dtype)
+            os_shape[a] = i
+
+            # Swap axes
+            output = output.swapaxes(a, -1)
+            os_shape[a], os_shape[-1] = os_shape[-1], os_shape[a]
+
+            # Oversampled IFFT
+            output = ifft(output, axes=[-1], norm=None)
+            output *= os_i / i**0.5
+            output = util.resize(output, os_shape)
+
+            # Calculate apodization
+            apod = (beta**2 - (np.pi * width * (idx - i // 2) / os_i)**2)**0.5
+            apod /= xp.sinh(apod)
+
+            # Apodize
+            output *= apod
+
+            # Swap back
+            output = output.swapaxes(a, -1)
+            os_shape[a], os_shape[-1] = os_shape[-1], os_shape[a]
+
+        return output
+
+
+def _fftc(input, oshape=None, axes=None, norm='ortho'):
+
+    ndim = input.ndim
+    axes = util._normalize_axes(axes, ndim)
+    device = backend.get_device(input)
+    xp = device.xp
+
+    if oshape is None:
+        oshape = input.shape
+
+    with device:
+        tmp = input
+        tshape = list(input.shape)
+        for a in axes:
+            i = oshape[a]
+            tshape[a] = i
+
+            tmp = tmp.swapaxes(a, -1)
+            tshape[a], tshape[-1] = tshape[-1], tshape[a]
+
+            tmp = util.resize(tmp, tshape)
+            tmp = xp.fft.ifftshift(tmp, axes=-1)
+            tmp = xp.fft.fft(tmp, axis=-1, norm=norm)
+            tmp = xp.fft.fftshift(tmp, axes=-1)
+
+            tmp = tmp.swapaxes(a, -1)
+            tshape[a], tshape[-1] = tshape[-1], tshape[a]
+
+        output = tmp
+
+    return output
+
+
+def _ifftc(input, oshape=None, axes=None, norm='ortho'):
+    ndim = input.ndim
+    axes = util._normalize_axes(axes, ndim)
+    device = backend.get_device(input)
+    xp = device.xp
+
+    if oshape is None:
+        oshape = input.shape
+
+    with device:
+        tmp = input
+        tshape = list(input.shape)
+        for a in axes:
+
+            i = oshape[a]
+            tshape[a] = i
+
+            tmp = tmp.swapaxes(a, -1)
+            tshape[a], tshape[-1] = tshape[-1], tshape[a]
+
+            tmp = util.resize(tmp, tshape)
+            tmp = xp.fft.ifftshift(tmp, axes=-1)
+            tmp = xp.fft.ifft(tmp, axis=-1, norm=norm)
+            tmp = xp.fft.fftshift(tmp, axes=-1)
+
+            tmp = tmp.swapaxes(a, -1)
+            tshape[a], tshape[-1] = tshape[-1], tshape[a]
+
+        output = tmp
+
+    return output
+
+
+def _kb(x, width, beta, dtype=np.complex):
+    return 1 / width * np.i0(beta * (1 - x**2)**0.5).astype(dtype)
+
+
+def _scale_coord(coord, shape, oversamp):
+    ndim = coord.shape[-1]
+    device = backend.get_device(coord)
+    scale = backend.to_device([_get_ugly_number(oversamp * i) / i for i in shape[-ndim:]], device)
+    shift = backend.to_device([_get_ugly_number(oversamp * i) // 2 for i in shape[-ndim:]], device)
+
+    with device:
+        coord = scale * coord + shift
+
+    return coord
+
+
+def _get_ugly_number(n):
+    """Get closest ugly number greater than n.
+
+    An ugly number is defined as a positive integer that is a multiple of 2, 3, and 5.
+    
+    Args:
+        n (int): Base number.
+
+    """
+    if n <= 1:
+        return n
+
+    ugly_nums = [1]
+    i2, i3, i5 = 0, 0, 0
+    while(True):
+
+        ugly_num = min(ugly_nums[i2] * 2,
+                       ugly_nums[i3] * 3,
+                       ugly_nums[i5] * 5)
+
+        if ugly_num >= n:
+            return ugly_num
+
+        ugly_nums.append(ugly_num)
+        if ugly_num == ugly_nums[i2] * 2:
+            i2 += 1
+        elif ugly_num == ugly_nums[i3] * 3:
+            i3 += 1
+        elif ugly_num == ugly_nums[i5] * 5:
+            i5 += 1
---
+++ sigpy/fourier_test.py
@@ -0,0 +1,230 @@
+import pickle
+import unittest
+import numpy as np
+import numpy.testing as npt
+from sigpy import fourier, util
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+class TestFourier(unittest.TestCase):
+
+    def test_fft(self):
+        input = np.array([0, 1, 0], dtype=np.complex)
+        npt.assert_allclose(fourier.fft(input),
+                            np.ones(3) / 3**0.5, atol=1e-5)
+
+        input = np.array([1, 1, 1], dtype=np.complex)
+        npt.assert_allclose(fourier.fft(input),
+                            [0, 3**0.5, 0], atol=1e-5)
+
+        input = util.randn([4, 5, 6])
+        npt.assert_allclose(fourier.fft(input),
+                            np.fft.fftshift(np.fft.fftn(
+                                np.fft.ifftshift(input), norm='ortho')),
+                            atol=1e-5)
+
+        input = np.array([0, 1, 0], dtype=np.complex)
+        npt.assert_allclose(fourier.fft(input, oshape=[5]),
+                            np.ones(5) / 5**0.5, atol=1e-5)
+
+    def test_fft_dtype(self):
+
+        for dtype in [np.complex64, np.complex128]:
+            input = np.array([0, 1, 0], dtype=dtype)
+            output = fourier.fft(input)
+
+            assert output.dtype == dtype
+
+    def test_ifft(self):
+        input = np.array([0, 1, 0], dtype=np.complex)
+        npt.assert_allclose(fourier.ifft(input),
+                            np.ones(3) / 3 ** 0.5, atol=1e-5)
+
+        input = np.array([1, 1, 1], dtype=np.complex)
+        npt.assert_allclose(fourier.ifft(input),
+                            [0, 3**0.5, 0], atol=1e-5)
+
+        input = util.randn([4, 5, 6])
+        npt.assert_allclose(fourier.ifft(input),
+                            np.fft.fftshift(np.fft.ifftn(
+                                np.fft.ifftshift(input), norm='ortho')),
+                            atol=1e-5)
+
+        input = np.array([0, 1, 0], dtype=np.complex)
+        npt.assert_allclose(fourier.ifft(input, oshape=[5]),
+                            np.ones(5) / 5**0.5, atol=1e-5)
+
+    def test_nufft(self):
+
+        # Check deltas
+        ishape = [3]
+        input = np.array([0, 1, 0], np.complex)  # delta
+        coord = np.array([[-1], [0], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft(input, coord),
+                            np.array([1.0, 1.0, 1.0]) / (3**0.5),
+                            atol=0.01, rtol=0.01)
+
+        ishape = [4]
+        input = np.array([0, 0, 1, 0], np.complex)  # delta
+        coord = np.array([[-2], [-1], [0], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft(input, coord),
+                            np.array([1.0, 1.0, 1.0, 1.0]) / (4**0.5),
+                            atol=0.01, rtol=0.01)
+
+        ishape = [5]
+        input = np.array([0, 0, 1, 0, 0], np.complex)  # delta
+        coord = np.array([[-2], [-1], [0], [1], [2]], np.float)
+
+        npt.assert_allclose(fourier.nufft(input, coord),
+                            np.ones(5) / (5**0.5),
+                            atol=0.01, rtol=0.01)
+
+        # Check shifted delta
+        ishape = [3]
+        input = np.array([0, 0, 1], np.complex)  # shifted delta
+        coord = np.array([[-1], [0], [1]], np.float)
+
+        w = np.exp(-1j * 2.0 * np.pi / 3.0)
+        npt.assert_allclose(fourier.nufft(input, coord),
+                            np.array([w.conjugate(), 1.0, w]) / (3**0.5),
+                            atol=0.01, rtol=0.01)
+
+        ishape = [4]
+        input = np.array([0, 0, 0, 1], np.complex)  # delta
+        coord = np.array([[-2], [-1], [0], [1]], np.float)
+
+        w = np.exp(-1j * 2.0 * np.pi / 4.0)
+        npt.assert_allclose(fourier.nufft(input, coord),
+                            np.array(
+                                [w.conjugate()**2, w.conjugate(), 1.0, w]) / (4**0.5),
+                            atol=0.01, rtol=0.01)
+
+    def test_nufft_nd(self):
+
+        for ndim in range(3):
+            ishape = [3, 1]
+
+            input = np.array([[0], [1], [0]], np.complex)
+            coord = np.array([[-1, 0],
+                              [0, 0],
+                              [1, 0]], np.float)
+
+            npt.assert_allclose(fourier.nufft(input, coord),
+                                np.array([1.0, 1.0, 1.0]) / 3**0.5,
+                                atol=0.01, rtol=0.01)
+
+    def test_nufft_adjoint(self):
+
+        # Check deltas
+        oshape = [3]
+        input = np.array([1.0, 1.0, 1.0], dtype=np.complex) / 3**0.5
+        coord = np.array([[-1], [0], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(input, coord, oshape),
+                            np.array([0, 1, 0]),
+                            atol=0.01, rtol=0.01)
+
+        oshape = [4]
+        input = np.array([1.0, 1.0, 1.0, 1.0], dtype=np.complex) / 4**0.5
+        coord = np.array([[-2], [-1], [0], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(input, coord, oshape),
+                            np.array([0, 0, 1, 0], np.complex),
+                            atol=0.01, rtol=0.01)
+
+        oshape = [5]
+        input = np.ones(5, dtype=np.complex) / 5**0.5
+        coord = np.array([[-2], [-1], [0], [1], [2]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(input, coord, oshape),
+                            np.array([0, 0, 1, 0, 0], np.complex),
+                            atol=0.01, rtol=0.01)
+
+        # Check shifted delta
+        oshape = [3]
+        w = np.exp(-1j * 2.0 * np.pi / 3.0)
+        input = np.array([w.conjugate(), 1.0, w]) / 3**0.5
+        coord = np.array([[-1], [0], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(input, coord, oshape),
+                            np.array([0, 0, 1], np.complex),
+                            atol=0.01, rtol=0.01)
+
+        oshape = [4]
+        w = np.exp(-1j * 2.0 * np.pi / 4.0)
+        input = np.array([w.conjugate()**2, w.conjugate(), 1.0, w]) / 4**0.5
+        coord = np.array([[-2], [-1], [0], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(input, coord, oshape),
+                            np.array([0, 0, 0, 1], np.complex),
+                            atol=0.01, rtol=0.01)
+
+    def test_nufft_adjoint_nd(self):
+
+        oshape = [3, 1]
+
+        input = np.array([1.0, 1.0, 1.0], dtype=np.complex) / 3**0.5
+        coord = np.array([[-1, 0],
+                          [0, 0],
+                          [1, 0]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(input, coord, oshape),
+                            np.array([[0], [1], [0]], np.complex),
+                            atol=0.01, rtol=0.01)
+
+    def test_nufft_normal(self):
+
+        # Check delta
+        oshape = [3]
+        input = np.array([0.0, 1.0, 0.0], dtype=np.complex)
+        coord = np.array([[-1], [0], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(fourier.nufft(input, coord), coord, oshape),
+                            np.array([0, 1, 0]),
+                            atol=0.01, rtol=0.01)
+
+        # Check delta scale
+        oshape = [3]
+        input = np.array([0.0, 1.0, 0.0], dtype=np.complex)
+        coord = np.array([[-1], [-0.5], [0], [0.5], [1]], np.float)
+
+        npt.assert_allclose(fourier.nufft_adjoint(fourier.nufft(input, coord),
+                                                  coord, oshape)[len(input) // 2],
+                            5 / 3,
+                            atol=0.01, rtol=0.01)
+
+    def test_nufft_ndft(self):
+
+        n = 5
+        w = np.exp(-1j * 2 * np.pi / n)
+        coord = np.array([[-2], [0], [0.1]])
+        w2 = w**-2
+        w1 = w**0.1
+        A = np.array([[w2**-2, w2**-1, 1, w2, w2**2],
+                      [1, 1, 1, 1, 1],
+                      [w1**-2, w1**-1, 1, w1, w1**2]]) / n**0.5
+
+        for i in range(n):
+            input = np.zeros(n, np.complex)
+            input[i] = 1
+            npt.assert_allclose(A[:, i], fourier.nufft(
+                input, coord), atol=0.01, rtol=0.01)
+
+        n = 6
+        w = np.exp(-1j * 2 * np.pi / n)
+        coord = np.array([[-2], [0], [0.1]])
+        w2 = w**-2
+        w1 = w**0.1
+        A = np.array([[w2**-3, w2**-2, w2**-1, 1, w2, w2**2],
+                      [1, 1, 1, 1, 1, 1],
+                      [w1**-3, w1**-2, w1**-1, 1, w1, w1**2]]) / n**0.5
+
+        for i in range(n):
+            input = np.zeros(n, np.complex)
+            input[i] = 1
+            npt.assert_allclose(A[:, i], fourier.nufft(
+                input, coord), atol=0.01, rtol=0.01)
