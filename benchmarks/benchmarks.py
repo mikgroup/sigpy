c1484696f3ca692da975072c950bcb38a8b61547
Frank Ong <frankong@berkeley.edu>
2018-12-18 15:33:40 -0800

    Add cupy to benchmark

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,53 +1,67 @@
 import numpy as np
 import sigpy as sp
+if sp.config.cupy_enabled:
+    import cupy as cp
 
 
 class FftSuite:
-    
-    def setup(self):
-        self.x = np.random.randn(100)
+    if sp.config.cupy_enabled:
+        params = [np, cp]
+    else:
+        params = [np]
+        
+    def setup(self, xp):
+        self.x = xp.random.randn(100)
 
-    def time_fft(self):
+    def time_fft(self, xp):
         y = sp.fft(self.x)
 
-    def time_fft_non_centered(self):
+    def time_fft_non_centered(self, xp):
         y = sp.fft(self.x, center=False)
 
-    def time_ifft(self):
+    def time_ifft(self, xp):
         y = sp.ifft(self.x)
 
-    def time_ifft_non_centered(self):
+    def time_ifft_non_centered(self, xp):
         y = sp.ifft(self.x, center=False)
 
         
 class NufftSuite:
+    if sp.config.cupy_enabled:
+        params = [np, cp]
+    else:
+        params = [np]
     
-    def setup(self):
-        self.x = np.random.randn(100)
-        self.coord = np.random.randn(100, 1)
+    def setup(self, xp):
+        self.x = xp.random.randn(100)
+        self.coord = xp.random.randn(100, 1)
 
-    def time_nufft(self):
+    def time_nufft(self, xp):
         y = sp.nufft(self.x, self.coord)
 
-    def time_nufft_adjoint(self):
+    def time_nufft_adjoint(self, xp):
         y = sp.nufft_adjoint(self.x, self.coord)
 
 
 class ThreshSuite:
+    if sp.config.cupy_enabled:
+        params = [np, cp]
+    else:
+        params = [np]
     
-    def setup(self):
-        self.x = np.random.randn(100)
+    def setup(self, xp):
+        self.x = xp.random.randn(100)
 
-    def time_soft_thresh(self):
+    def time_soft_thresh(self, xp):
         y = sp.soft_thresh(1e-3, self.x)
 
-    def time_hard_thresh(self):
+    def time_hard_thresh(self, xp):
         y = sp.hard_thresh(1e-3, self.x)
 
-    def time_elitist_thresh(self):
+    def time_elitist_thresh(self, xp):
         y = sp.elitist_thresh(1e-3, self.x)
 
-    def time_l1_proj(self):
+    def time_l1_proj(self, xp):
         y = sp.l1_proj(1, self.x)
 
 
