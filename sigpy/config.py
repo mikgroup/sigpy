397bee25efd1b7543585e38ee6b6fede4b260f20
Frank Ong <frankong@berkeley.edu>
2019-02-28 21:39:08 -0800

    Fix cudnn and nccl config flags

--- sigpy/config.py
+++ sigpy/config.py
@@ -7,6 +7,11 @@ This module contains flags to turn on and off optional modules.
 import importlib
 
 cupy_enabled = importlib.util.find_spec("cupy") is not None
-cudnn_enabled = importlib.util.find_spec("cupy.cuda.cudnn") is not None
-nccl_enabled = importlib.util.find_spec("cupy.cuda.nccl") is not None
+if cupy_enabled:
+    cudnn_enabled = importlib.util.find_spec("cupy.cuda.cudnn") is not None
+    nccl_enabled = importlib.util.find_spec("cupy.cuda.nccl") is not None
+else:
+    cudnn_anbled = False
+    nccl_enabled = False
+
 mpi4py_enabled = importlib.util.find_spec("mpi4py") is not None
