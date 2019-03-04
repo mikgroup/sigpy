14e434b19b1ee3dfeae7ac53e6cec731e491f59e
Frank Ong <frankong@berkeley.edu>
2019-03-03 17:12:24 -0800

    Fix importlib import error

--- sigpy/config.py
+++ sigpy/config.py
@@ -4,14 +4,14 @@
 This module contains flags to turn on and off optional modules.
 
 """
-import importlib
+from importlib import util
 
-cupy_enabled = importlib.util.find_spec("cupy") is not None
+cupy_enabled = util.find_spec("cupy") is not None
 if cupy_enabled:
-    cudnn_enabled = importlib.util.find_spec("cupy.cuda.cudnn") is not None
-    nccl_enabled = importlib.util.find_spec("cupy.cuda.nccl") is not None
+    cudnn_enabled = util.find_spec("cupy.cuda.cudnn") is not None
+    nccl_enabled = util.find_spec("cupy.cuda.nccl") is not None
 else:
     cudnn_enabled = False
     nccl_enabled = False
 
-mpi4py_enabled = importlib.util.find_spec("mpi4py") is not None
+mpi4py_enabled = util.find_spec("mpi4py") is not None
