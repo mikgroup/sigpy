Fix NewtonsMethod scalar calculation

tests/test_alg.py
@@ -164,7 +164,8 @@ class TestAlg(unittest.TestCase):
             return gradf_x
 
         def inv_hessf(x):
-            return lambda x: np.linalg.pinv(A.T @ A) @ x
+            I = np.eye(n)
+            return lambda x: np.linalg.pinv(A.T @ A + lamda * I) @ x
         for beta in [1, 0.5]:
             with self.subTest(beta=beta):
                 if beta < 1: