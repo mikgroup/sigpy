cdb3590335fdbb4279a9bccd0f4b95a454a2c2b1
Frank Ong <frankong@berkeley.edu>
2019-04-16 15:25:58 -0700

    Bump version: 0.1.7 → 0.1.8

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Frank Ong'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.7'
+release = '0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
