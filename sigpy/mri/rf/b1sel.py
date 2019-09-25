24d78e77b6d7e956ec2a03a19794fc9c3efec2e0
Will Grissom <wgrissom@gmail.com>
2019-09-25 15:05:14 -0500

    added hadamard rf filters and hadamard b1-selective pulses

--- sigpy/mri/rf/b1sel.py
+++ sigpy/mri/rf/b1sel.py
@@ -8,7 +8,7 @@ from sigpy.mri.rf import slr as slr
 __all__ = ['dzb1rf', 'dzb1gSliderrf']
 
 def dzb1rf(dt = 2e-6, tb = 4, ptype = 'st', flip = np.pi/6, pbw = 0.3, pbc = 2,
-    d1 = 0.01, d2 = 0.01, os = 8):
+    d1 = 0.01, d2 = 0.01, os = 8, splitAndReflect = True):
 
     # design a B1-selective excitation pulse, following Grissom JMR 2014
     # pbw = width of passband in Gauss
@@ -64,8 +64,11 @@ def dzb1rf(dt = 2e-6, tb = 4, ptype = 'st', flip = np.pi/6, pbw = 0.3, pbc = 2,
         t = np.arange(0, n)*T/n - T/2
         h = 2*h*np.sin(om*t)
 
-    # split and flip fm waveform to improve large-tip accuracy
-    dom = np.concatenate((h[n//2::-1], h, h[n:n//2:-1]))/2
+    if splitAndReflect:
+        # split and flip fm waveform to improve large-tip accuracy
+        dom = np.concatenate((h[n//2::-1], h, h[n:n//2:-1]))/2
+    else:
+        dom = np.concatenate((0*h[n//2::-1], h, 0*h[n:n//2:-1]))
 
     # scale to target flip, convert to Hz
     dom = dom*flip/(2*np.pi*dt)
@@ -77,7 +80,7 @@ def dzb1rf(dt = 2e-6, tb = 4, ptype = 'st', flip = np.pi/6, pbw = 0.3, pbc = 2,
 
 
 def dzb1gSliderrf(dt = 2e-6, G = 5, tb = 12, ptype = 'st', flip = np.pi/6,
-    pbw = 0.5, pbc = 2, d1 = 0.01, d2 = 0.01):
+    pbw = 0.5, pbc = 2, d1 = 0.01, d2 = 0.01, splitAndReflect = True):
 
     # design B1-selective excitation gSlider pulses,
     # following Grissom JMR 2014
@@ -103,14 +106,65 @@ def dzb1gSliderrf(dt = 2e-6, G = 5, tb = 12, ptype = 'st', flip = np.pi/6,
     dom = np.zeros((2*n, G))
     for Gind in range(1,G+1):
         # design filter
-        h = bsf*slr.dzgSliderB(n, G, Gind, tb, d1, d2, np.pi)
+        h = bsf*slr.dzgSliderB(n, G, Gind, tb, d1, d2, np.pi, n//4)
         #if ptype == 'ex':
         #h = slr.b2rf(h)
         # modulate filter to center and add it to a time-reversed and modulated
         # copy, then take the imaginary part to get an odd filter
         h = np.imag(h*np.exp(1j*om*t) - h[n::-1]*np.exp(1j*-om*t))
-        # split and flip fm waveform to improve large-tip accuracy
-        dom[:, Gind-1] = np.concatenate((h[n//2::-1], h, h[n:n//2:-1]))/2
+        if splitAndReflect:
+            # split and flip fm waveform to improve large-tip accuracy
+            dom[:, Gind-1] = np.concatenate((h[n//2::-1], h, h[n:n//2:-1]))/2
+        else:
+            dom[:, Gind-1] = np.concatenate((0*h[n//2::-1], h, 0*h[n:n//2:-1]))
+        # build am waveform
+        om1[:, Gind-1] = np.concatenate((-np.ones(n//2), np.ones(n),
+            -np.ones(n//2)))
+
+    # scale to target flip, convert to Hz
+    dom = dom/(2*np.pi*dt)
+
+    return om1, dom
+
+
+def dzb1Hadamardrf(dt = 2e-6, G = 8, tb = 16, ptype = 'st', flip = np.pi/6,
+    pbw = 2, pbc = 2, d1 = 0.01, d2 = 0.01, splitAndReflect = True):
+
+    # design B1-selective excitation gSlider pulses,
+    # following Grissom JMR 2014
+    # pbw = width of passband in Gauss
+    # pbc = center of passband in Gauss
+
+    # calculate beta filter ripple
+    [bsf, d1, d2] = slr.calcRipples(ptype, d1, d2)
+    #if ptype == 'st':
+    bsf = flip
+
+    # calculate pulse duration
+    B = 4257*pbw
+    T = tb/B
+
+    # calculate number of samples in pulse
+    n = np.int(np.ceil(T/dt/2)*2)
+
+    om = 2*np.pi*4257*pbc # modulation frequency to center profile at pbc gauss
+    t = np.arange(0, n)*T/n - T/2
+
+    om1 = np.zeros((2*n, G))
+    dom = np.zeros((2*n, G))
+    for Gind in range(1,G+1):
+        # design filter
+        h = bsf*slr.dzHadamardB(n, G, Gind, tb, d1, d2, n//4)
+        #if ptype == 'ex':
+        #h = slr.b2rf(h)
+        # modulate filter to center and add it to a time-reversed and modulated
+        # copy, then take the imaginary part to get an odd filter
+        h = np.imag(h*np.exp(1j*om*t) - h[n::-1]*np.exp(1j*-om*t))
+        if splitAndReflect:
+            # split and flip fm waveform to improve large-tip accuracy
+            dom[:, Gind-1] = np.concatenate((h[n//2::-1], h, h[n:n//2:-1]))/2
+        else:
+            dom[:, Gind-1] = np.concatenate((0*h[n//2::-1], h, 0*h[n:n//2:-1]))
         # build am waveform
         om1[:, Gind-1] = np.concatenate((-np.ones(n//2), np.ones(n),
             -np.ones(n//2)))
