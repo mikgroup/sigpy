1eb4fc577acd084340c2903494d7ac5f422f26af
jonbmartin <jon.bach.martin@gmail.com>
2019-10-19 22:02:07 -0500

    Code cleanup of optcont and b1sel

--- sigpy/mri/rf/b1sel.py
+++ sigpy/mri/rf/b1sel.py
@@ -59,15 +59,14 @@ def dzb1rf(dt=2e-6, tb=4, ptype='st', flip=np.pi / 6, pbw=0.3, pbc=2,
 
         if splitAndReflect:
             # split and flip fm waveform to improve large-tip accuracy
-            #fm = np.concatenate((h[n//2::-1], h, h[n:n//2:-1]))/2
-            fm = np.concatenate((-h[n//2:n:1], h, -h[0:n//2:1]))/2
+            fm = np.concatenate((-h[n // 2:n:1], h, -h[0:n // 2:1])) / 2
         else:
-            fm = np.concatenate((0*h[n//2::-1], h, 0*h[n:n//2:-1]))
+            fm = np.concatenate((0 * h[n // 2::-1], h, 0 * h[n:n // 2:-1]))
 
         # scale to target flip, convert to Hz
-        fm = fm*flip/(2*np.pi*dt)
+        fm = fm * flip / (2 * np.pi * dt)
 
-    else: # normal design
+    else:  # normal design
 
         # design filter
         h = slr.dzls(n, tb, d1, d2)
@@ -76,33 +75,30 @@ def dzb1rf(dt=2e-6, tb=4, ptype='st', flip=np.pi / 6, pbw=0.3, pbc=2,
         fm = buildfm(h, pbc, T, flip, dt, splitAndReflect)
 
     # build am waveform
-    am = np.concatenate((-np.ones(n//2), np.ones(n), -np.ones(n//2)))
+    am = np.concatenate((-np.ones(n // 2), np.ones(n), -np.ones(n // 2)))
 
     return am, fm
 
 
-def buildfm(h, pbc, T, flip, dt, splitAndReflect = True):
-
+def buildfm(h, pbc, T, flip, dt, splitAndReflect=True):
     n = np.size(h)
 
     # dual-band-modulate the filter
-    om = 2*np.pi*4257*pbc # modulation frequency
-    t = np.arange(0, n)*T/n #- T/2
-    t -= (t[n//2-2] + t[n//2-1])/2 # center the time vector
-    #h = 2*h*np.sin(om*t)
+    om = 2 * np.pi * 4257 * pbc  # modulation frequency
+    t = np.arange(0, n) * T / n  # - T/2
+    t -= (t[n // 2 - 2] + t[n // 2 - 1]) / 2  # center the time vector
     # modulate filter to center and add it to a time-reversed and modulated
     # copy, then take the imaginary part to get an odd filter
-    h = np.imag(h*np.exp(1j*om*t) - h[n::-1]*np.exp(1j*-om*t))
+    h = np.imag(h * np.exp(1j * om * t) - h[n::-1] * np.exp(1j * -om * t))
 
     if splitAndReflect:
         # split and flip fm waveform to improve large-tip accuracy
-        #fm = np.concatenate((h[n//2::-1], h, h[n:n//2:-1]))/2
-        fm = np.concatenate((-h[n//2:n:1], h, -h[0:n//2:1]))/2
+        fm = np.concatenate((-h[n // 2:n:1], h, -h[0:n // 2:1])) / 2
     else:
-        fm = np.concatenate((0*h[n//2::-1], h, 0*h[n:n//2:-1]))
+        fm = np.concatenate((0 * h[n // 2::-1], h, 0 * h[n:n // 2:-1]))
 
     # scale to target flip, convert to Hz
-    fm = fm*flip/(2*np.pi*dt)
+    fm = fm * flip / (2 * np.pi * dt)
 
     return fm
 
@@ -116,8 +112,6 @@ def dzb1gSliderrf(dt=2e-6, G=5, tb=12, ptype='st', flip=np.pi / 6,
 
     # calculate beta filter ripple
     [bsf, d1, d2] = slr.calcRipples(ptype, d1, d2)
-    #if ptype == 'st':
-    #bsf = flip
 
     # calculate pulse duration
     B = 4257 * pbw
@@ -126,17 +120,17 @@ def dzb1gSliderrf(dt=2e-6, G=5, tb=12, ptype='st', flip=np.pi / 6,
     # calculate number of samples in pulse
     n = np.int(np.ceil(T / dt / 2) * 2)
 
-    am = np.zeros((2*n, G))
-    fm = np.zeros((2*n, G))
-    for Gind in range(1, G+1):
+    am = np.zeros((2 * n, G))
+    fm = np.zeros((2 * n, G))
+    for Gind in range(1, G + 1):
         # design filter
-        h = slr.dzgSliderB(n, G, Gind, tb, d1, d2, np.pi, n//4)
-        #if ptype == 'ex':
-        #h = slr.b2rf(h)
-        fm[:, Gind-1] = buildfm(h, pbc, T, flip, dt, splitAndReflect)
+        h = slr.dzgSliderB(n, G, Gind, tb, d1, d2, np.pi, n // 4)
+        # if ptype == 'ex':
+        # h = slr.b2rf(h)
+        fm[:, Gind - 1] = buildfm(h, pbc, T, flip, dt, splitAndReflect)
         # build am waveform
-        am[:, Gind-1] = np.concatenate((-np.ones(n//2), np.ones(n),
-            -np.ones(n//2)))
+        am[:, Gind - 1] = np.concatenate((-np.ones(n // 2), np.ones(n),
+                                          -np.ones(n // 2)))
 
     return am, fm
 
@@ -150,8 +144,6 @@ def dzb1Hadamardrf(dt=2e-6, G=8, tb=16, ptype='st', flip=np.pi / 6,
 
     # calculate beta filter ripple
     [bsf, d1, d2] = slr.calcRipples(ptype, d1, d2)
-    #if ptype == 'st':
-    #bsf = flip
 
     # calculate pulse duration
     B = 4257 * pbw
@@ -160,16 +152,14 @@ def dzb1Hadamardrf(dt=2e-6, G=8, tb=16, ptype='st', flip=np.pi / 6,
     # calculate number of samples in pulse
     n = np.int(np.ceil(T / dt / 2) * 2)
 
-    am = np.zeros((2*n, G))
-    fm = np.zeros((2*n, G))
-    for Gind in range(1, G+1):
+    am = np.zeros((2 * n, G))
+    fm = np.zeros((2 * n, G))
+    for Gind in range(1, G + 1):
         # design filter
-        h = slr.dzHadamardB(n, G, Gind, tb, d1, d2, n//4)
-        #if ptype == 'ex':
-        #h = slr.b2rf(h)
-        fm[:, Gind-1] = buildfm(h, pbc, T, flip, dt, splitAndReflect)
+        h = slr.dzHadamardB(n, G, Gind, tb, d1, d2, n // 4)
+        fm[:, Gind - 1] = buildfm(h, pbc, T, flip, dt, splitAndReflect)
         # build am waveform
-        am[:, Gind-1] = np.concatenate((-np.ones(n//2), np.ones(n),
-            -np.ones(n//2)))
+        am[:, Gind - 1] = np.concatenate((-np.ones(n // 2), np.ones(n),
+                                          -np.ones(n // 2)))
 
     return am, fm
--- sigpy/mri/rf/optcont.py
+++ sigpy/mri/rf/optcont.py
@@ -18,33 +18,32 @@ def blochsim(rf, x, g):
         for mm in range(0, xp.size(rf), 1):  # loop over time
 
             # apply RF
-            C = xp.cos(xp.abs(rf[mm])/2)
-            S = 1j*xp.exp(1j*xp.angle(rf[mm]))*xp.sin(xp.abs(rf[mm])/2)
-            at = a*C - b*xp.conj(S)
-            bt = a*S + b*C
+            C = xp.cos(xp.abs(rf[mm]) / 2)
+            S = 1j * xp.exp(1j * xp.angle(rf[mm])) * xp.sin(xp.abs(rf[mm]) / 2)
+            at = a * C - b * xp.conj(S)
+            bt = a * S + b * C
             a = at
             b = bt
 
             # apply gradient
             if g.ndim > 1:
-                z = xp.exp(-1j*x@g[mm, :])
+                z = xp.exp(-1j * x @ g[mm, :])
             else:
-                z = xp.exp(-1j*x*g[mm])
-            b = b*z
+                z = xp.exp(-1j * x * g[mm])
+            b = b * z
 
         # apply total phase accrual
         if g.ndim > 1:
-            z = xp.exp(1j/2*x@xp.sum(g, 0))
+            z = xp.exp(1j / 2 * x @ xp.sum(g, 0))
         else:
-            z = xp.exp(1j/2*x*xp.sum(g))
-        a = a*z
-        b = b*z
+            z = xp.exp(1j / 2 * x * xp.sum(g))
+        a = a * z
+        b = b * z
 
         return a, b
 
 
 def deriv(rf, x, g, auxa, auxb, af, bf):
-
     device = backend.get_device(rf)
     xp = device.xp
     with device:
@@ -52,42 +51,42 @@ def deriv(rf, x, g, auxa, auxb, af, bf):
         ar = xp.ones(xp.shape(af), dtype=complex)
         br = xp.zeros(xp.shape(bf), dtype=complex)
 
-        for mm in range(xp.size(rf)-1, -1, -1):
+        for mm in range(xp.size(rf) - 1, -1, -1):
 
             # calculate gradient blip phase
             if g.ndim > 1:
-                z = xp.exp(1j/2*x@g[mm, :])
+                z = xp.exp(1j / 2 * x @ g[mm, :])
             else:
-                z = xp.exp(1j/2*x*g[mm])
+                z = xp.exp(1j / 2 * x * g[mm])
 
             # strip off gradient blip from forward sim
-            af = af*xp.conj(z)
-            bf = bf*z
+            af = af * xp.conj(z)
+            bf = bf * z
 
             # add gradient blip to backward sim
-            ar = ar*z
-            br = br*z
+            ar = ar * z
+            br = br * z
 
             # strip off the curent rf rotation from forward sim
-            C = xp.cos(xp.abs(rf[mm])/2)
-            S = 1j*xp.exp(1j*xp.angle(rf[mm]))*xp.sin(xp.abs(rf[mm])/2)
-            at = af*C + bf*xp.conj(S)
-            bt = -af*S + bf*C
+            C = xp.cos(xp.abs(rf[mm]) / 2)
+            S = 1j * xp.exp(1j * xp.angle(rf[mm])) * xp.sin(xp.abs(rf[mm]) / 2)
+            at = af * C + bf * xp.conj(S)
+            bt = -af * S + bf * C
             af = at
             bf = bt
 
             # calculate derivatives wrt rf[mm]
-            db1 = xp.conj(1j/2*br*bf)*auxb
-            db2 = xp.conj(1j/2*af)*ar*auxb
+            db1 = xp.conj(1j / 2 * br * bf) * auxb
+            db2 = xp.conj(1j / 2 * af) * ar * auxb
             drf[mm] = xp.sum(db2 + xp.conj(db1))
             if auxa is not None:
-                da1 = xp.conj(1j/2*bf*ar)*auxa
-                da2 = 1j/2*xp.conj(af)*br*auxa
+                da1 = xp.conj(1j / 2 * bf * ar) * auxa
+                da2 = 1j / 2 * xp.conj(af) * br * auxa
                 drf[mm] += xp.sum(da2 + xp.conj(da1))
 
             # add current rf rotation to backward sim
-            art = ar*C - xp.conj(br)*S
-            brt = br*C + xp.conj(ar)*S
+            art = ar * C - xp.conj(br) * S
+            brt = br * C + xp.conj(ar) * S
             ar = art
             br = brt
 
