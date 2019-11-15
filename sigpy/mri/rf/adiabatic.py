8e0c69771142fe6f64b975dcc3ead406ade73113
jonbmartin <jon.bach.martin@gmail.com>
2019-11-14 18:06:35 -0600

    adiabatic pulse designers

---
+++ sigpy/mri/rf/adiabatic.py
@@ -0,0 +1,115 @@
+"""Adiabatic Pulse Design functions.
+
+"""
+import numpy as np
+
+__all__ = ['bir4', 'hypsec', 'wurst']
+
+
+def bir4(n, beta, kappa, theta, dw0):
+    r"""Generate a BIR-4 adiabatic pulse.
+
+    BIR-4 is equivalent to two BIR-1 pulses back-to-back.
+
+    Args:
+        n (int): number of samples (should be a multiple of 4).
+        beta (float): AM waveform parameter.
+        kappa (float): FM waveform parameter.
+        theta (float): flip angle in radians.
+        dw0: FM waveform scaling (radians/s).
+
+    Returns:
+        2-element tuple containing
+
+        - **a** (*array*): AM waveform.
+        - **om** (*array*): FM waveform (radians/s).
+
+    References:
+        Staewen, R.S. et al. (1990). '3-D FLASH Imaging using a single surface
+        coil and a new adiabatic pulse, BIR-4'.
+        Invest. Radiology, 25:559-567.
+     """
+
+    dphi = np.pi + theta/2
+
+    t = np.arange(0, n) / n
+
+    a1 = np.tanh(beta * (1 - 4 * t[:n // 4]))
+    a2 = np.tanh(beta * (4 * t[n // 4:n // 2] - 1))
+    a3 = np.tanh(beta * (3 - 4 * t[n // 2:3 * n // 4]))
+    a4 = np.tanh(beta * (4 * t[3 * n // 4:] - 3))
+
+    a = np.concatenate((a1, a2, a3, a4)).astype(complex)
+    a[n // 4:3 * n // 4] = a[n // 4:3 * n // 4] * np.exp(1j * dphi)
+
+    om1 = dw0 * np.tan(kappa * 4 * t[:n // 4]) / np.tan(kappa)
+    om2 = dw0 * np.tan(kappa * (4 * t[n // 4:n // 2] - 2)) / np.tan(kappa)
+    om3 = dw0 * np.tan(kappa * (4 * t[n // 2:3 * n // 4] - 2)) / np.tan(kappa)
+    om4 = dw0 * np.tan(kappa * (4 * t[3 * n // 4:] - 4)) / np.tan(kappa)
+
+    om = np.concatenate((om1, om2, om3, om4))
+
+    return a, om
+
+
+def hypsec(n=512, beta=800, mu=4.9, T=0.012):
+    r"""Generate a hyperbolic secant adiabatic pulse.
+
+    mu * beta becomes the amplitude of the frequency sweep
+
+    Args:
+        n (int): number of samples (should be a multiple of 4).
+        beta (float): AM waveform parameter.
+        mu (float): a constant, determines amplitude of frequency sweep.
+        T (float): pulse time (s).
+
+    Returns:
+        2-element tuple containing
+
+        - **a** (*array*): AM waveform.
+        - **om** (*array*): FM waveform (radians/s).
+
+    References:
+        Baum, J., Tycko, R. and Pines, A. (1985). 'Broadband and adiabatic
+        inversion of a two-level system by phase-modulated pulses'.
+        Phys. Rev. A., 32:3435-3447.
+     """
+
+    t = np.arange(-n // 2, n // 2) / n * T
+
+    a = np.cosh(beta * t) ** -1
+    om = -mu * beta * np.tanh(beta * t)
+
+    return a, om
+
+
+def wurst(n=512, N_fac=40, bw=40e3, T=2e-3):
+    r"""Generate a WURST (wideband, uniform rate, smooth truncation) adiabatic
+     inversion pulse
+
+    Args:
+        n (int): number of samples (should be a multiple of 4).
+        N_fac (int): power to exponentiate to within AM term. ~20 or greater is
+         typical.
+        bw (float): pulse bandwidth.
+        T (float): pulse time (s).
+
+
+    Returns:
+        2-element tuple containing
+
+        - **a** (*array*): AM waveform.
+        - **om** (*array*): FM waveform (radians/s).
+
+    References:
+        Kupce, E. and Freeman, R. (1995). 'Stretched Adiabatic Pulses for
+        Broadband Spin Inversion'.
+        J. Magn. Reson. Ser. A., 117:246-256.
+     """
+
+    t = np.arange(0, n) * T / n
+
+    a = 1 - np.power(np.abs(np.cos(np.pi * t / T)), N_fac)
+    om = np.linspace(-bw / 2, bw / 2, n) * 2 * np.pi
+
+    return a, om
