2132915a925d0bc86e77164bb4bbfd433f41710f
Frank Ong <frankongh@gmail.com>
2020-02-02 18:11:38 +0000

    Remove logo

--- docs/conf.py
+++ docs/conf.py
@@ -83,14 +83,13 @@ pygments_style = 'sphinx'
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_logo = 'figures/sigpy_logo_white.png'
+html_logo = ''
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'navigation_depth': 1,
-                      'logo_only': True}
+html_theme_options = {'navigation_depth': 1}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
