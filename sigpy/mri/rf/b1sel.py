c1b31ac6a328228bf468e77a2a9b350154799d9e
jonbmartin <jon.bach.martin@gmail.com>
2020-06-03 12:42:51 -0400

    clean up b1sel formatting

--- sigpy/mri/rf/b1sel.py
+++ sigpy/mri/rf/b1sel.py
@@ -8,7 +8,7 @@ from sigpy.mri.rf.util import dinf
 __all__ = ['dz_b1_rf', 'dz_b1_gslider_rf', 'dz_b1_hadamard_rf']
 
 
-def dz_b1_rf(dt=2e-6, tb=4, ptype='st', flip=np.pi/6, pbw=0.3,
+def dz_b1_rf(dt=2e-6, tb=4, ptype='st', flip=np.pi / 6, pbw=0.3,
              pbc=2, d1=0.01, d2=0.01, os=8, split_and_reflect=True):
     """Design a B1-selective excitation pulse following Grissom JMR 2014
 
@@ -43,11 +43,11 @@ def dz_b1_rf(dt=2e-6, tb=4, ptype='st', flip=np.pi/6, pbw=0.3,
     [bsf, d1, d2] = slr.calcRipples(ptype, d1, d2)
 
     # calculate pulse duration
-    B = 4257 * pbw
-    T = tb / B
+    b = 4257 * pbw
+    pulse_len = tb / b
 
     # calculate number of samples in pulse
-    n = np.int(np.ceil(T / dt / 2) * 2)
+    n = np.int(np.ceil(pulse_len / dt / 2) * 2)
 
     if pbc == 0:
         # we want passband as close to zero as possible.
@@ -90,7 +90,7 @@ def dz_b1_rf(dt=2e-6, tb=4, ptype='st', flip=np.pi/6, pbw=0.3,
 
         # dual-band-modulate the filter
         om = 2 * np.pi * 4257 * pbc  # modulation frequency
-        t = np.arange(0, n) * T / n - T / 2
+        t = np.arange(0, n) * pulse_len / n - pulse_len / 2
         h = 2 * h * np.sin(om * t)
 
     if split_and_reflect:
@@ -108,14 +108,14 @@ def dz_b1_rf(dt=2e-6, tb=4, ptype='st', flip=np.pi/6, pbw=0.3,
     return om1, dom
 
 
-def dz_b1_gslider_rf(dt=2e-6, G=5, tb=12, ptype='st', flip=np.pi/6,
+def dz_b1_gslider_rf(dt=2e-6, g=5, tb=12, ptype='st', flip=np.pi / 6,
                      pbw=0.5, pbc=2, d1=0.01, d2=0.01, split_and_reflect=True):
     """Design a B1-selective excitation gSlider pulse following Grissom JMR
     2014.
 
     Args:
         dt (float): hardware sampling dwell time in s.
-        G (int): number of slabs to be acquired.
+        g (int): number of slabs to be acquired.
         tb (int): time-bandwidth product.
         ptype (string): pulse type, 'st', 'ex', 'se', 'inv', or 'sat'.
         flip (float): flip angle, in radians.
@@ -146,33 +146,33 @@ def dz_b1_gslider_rf(dt=2e-6, G=5, tb=12, ptype='st', flip=np.pi/6,
     bsf = flip
 
     # calculate pulse duration
-    B = 4257 * pbw
-    T = tb / B
+    b = 4257 * pbw
+    pulse_len = tb / b
 
     # calculate number of samples in pulse
-    n = np.int(np.ceil(T / dt / 2) * 2)
+    n = np.int(np.ceil(pulse_len / dt / 2) * 2)
 
-    om = 2 * np.pi * 4257 * pbc  # modulation frequency to center profile at pbc
-    t = np.arange(0, n) * T / n - T / 2
+    om = 2 * np.pi * 4257 * pbc  # modulation freq to center profile at pbc
+    t = np.arange(0, n) * pulse_len / n - pulse_len / 2
 
-    om1 = np.zeros((2 * n, G))
-    dom = np.zeros((2 * n, G))
-    for Gind in range(1, G + 1):
+    om1 = np.zeros((2 * n, g))
+    dom = np.zeros((2 * n, g))
+    for gind in range(1, g + 1):
         # design filter
-        h = bsf*slr.dzgSliderB(n, G, Gind, tb, d1, d2, np.pi, n // 4)
+        h = bsf*slr.dzgSliderB(n, g, gind, tb, d1, d2, np.pi, n // 4)
 
         # modulate filter to center and add it to a time-reversed and modulated
         # copy, then take the imaginary part to get an odd filter
         h = np.imag(h * np.exp(1j * om * t) - h[n::-1] * np.exp(1j * -om * t))
         if split_and_reflect:
             # split and flip fm waveform to improve large-tip accuracy
-            dom[:, Gind - 1] = np.concatenate((h[n // 2::-1],
+            dom[:, gind - 1] = np.concatenate((h[n // 2::-1],
                                                h, h[n:n // 2:-1])) / 2
         else:
-            dom[:, Gind - 1] = np.concatenate((0*h[n // 2::-1],
-                                             h, 0*h[n:n // 2:-1]))
+            dom[:, gind - 1] = np.concatenate((0 * h[n // 2::-1],
+                                              h, 0 * h[n:n // 2:-1]))
         # build am waveform
-        om1[:, Gind - 1] = np.concatenate((-np.ones(n // 2), np.ones(n),
+        om1[:, gind - 1] = np.concatenate((-np.ones(n // 2), np.ones(n),
                                           -np.ones(n // 2)))
 
     # scale to target flip, convert to Hz
@@ -181,7 +181,7 @@ def dz_b1_gslider_rf(dt=2e-6, G=5, tb=12, ptype='st', flip=np.pi/6,
     return om1, dom
 
 
-def dz_b1_hadamard_rf(dt=2e-6, g=8, tb=16, ptype='st', flip=np.pi/6,
+def dz_b1_hadamard_rf(dt=2e-6, g=8, tb=16, ptype='st', flip=np.pi / 6,
                       pbw=2, pbc=2, d1=0.01, d2=0.01, split_and_reflect=True):
     """Design a B1-selective Hadamard-encoded pulse following Grissom JMR
     2014.
@@ -217,15 +217,15 @@ def dz_b1_hadamard_rf(dt=2e-6, g=8, tb=16, ptype='st', flip=np.pi/6,
     bsf = flip
 
     # calculate pulse duration
-    B = 4257 * pbw
-    T = tb / B
+    b = 4257 * pbw
+    pulse_len = tb / b
 
     # calculate number of samples in pulse
-    n = np.int(np.ceil(T / dt / 2) * 2)
+    n = np.int(np.ceil(pulse_len / dt / 2) * 2)
 
     # modulation frequency to center profile at pbc gauss
     om = 2 * np.pi * 4257 * pbc
-    t = np.arange(0, n) * T / n - T / 2
+    t = np.arange(0, n) * pulse_len / n - pulse_len / 2
 
     om1 = np.zeros((2 * n, g))
     dom = np.zeros((2 * n, g))
@@ -247,7 +247,7 @@ def dz_b1_hadamard_rf(dt=2e-6, g=8, tb=16, ptype='st', flip=np.pi/6,
                                               0 * h[n:n // 2:-1]))
         # build am waveform
         om1[:, gind - 1] = np.concatenate((-np.ones(n // 2), np.ones(n),
-                                        -np.ones(n // 2)))
+                                          -np.ones(n // 2)))
 
     # scale to target flip, convert to Hz
     dom = dom / (2 * np.pi * dt)
