f32072e383a7410fa07febd39382080837f9f905
Zhengguo Tan <zhengguo.tan@gmail.com>
2023-01-05 22:03:00 +0100

    coding style

--- sigpy/mri/dims.py
+++ sigpy/mri/dims.py
@@ -5,5 +5,5 @@ DIM_X = -1
 DIM_Y = -2
 DIM_Z = -3
 DIM_COIL = -4
-DIM_ECHO = -5  # For diffusion MRI, it stores the shots per DWI 
+DIM_ECHO = -5  # For diffusion MRI, it stores the shots per DWI
 DIM_TIME = -6  # For diffusion MRI, it stores the diffusion encodings
