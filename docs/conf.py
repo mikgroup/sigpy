5da0e8605f166be41e520ef0ef913482487611d8
Shreya Ramachandran <shreyar@berkeley.edu>
2024-12-26 19:42:14 -0800

    Bump version: 0.1.26 → 0.1.27

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Frank Ong"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.1.25"
+release = "0.1.27"
 
 
 # -- General configuration ---------------------------------------------------
